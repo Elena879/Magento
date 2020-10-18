139bacca63303424a6c72700ceb3c4fd10c09057
Elena Zabolotnaya <elena.zabolotnaya18@gmail.com>
2020-10-18 15:21:52 +0300

    #3 Pipeline deployment: added styles for Sample Data

--- app/etc/env.prod.php
+++ app/etc/env.prod.php
@@ -91,8 +91,8 @@ return [
                     'base_link_url' => '{{secure_base_url}}',
                     'base_static_url' => 'https://elena-zabolotnaya.local/static/',
                     'base_media_url' => 'https://elena-zabolotnaya.local/media/'
-                ],
-            ],
+                ]
+            ]
         ],
         'websites' => [
             'additional_website' => [
