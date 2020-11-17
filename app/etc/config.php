bc1a5a8bd59940ea143661bd2408a9b24e4bd543
Elena Zabolotnaya <elena.zabolotnaya18@gmail.com>
2020-11-17 19:36:13 +0200

    #6 Layout: moved product Wishlist/Compare/Email links and removed the breadcrumbs from all CMS pages

--- app/etc/config.php
+++ app/etc/config.php
@@ -308,7 +308,7 @@ return [
         'Klarna_Kp' => 1,
         'Klarna_Onsitemessaging' => 1,
         'Klarna_KpGraphQl' => 1,
-        'Lena_LessDemo' => 1,
+        'Lena_LayoutDebug' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
