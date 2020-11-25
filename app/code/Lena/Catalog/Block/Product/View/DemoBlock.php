fd6fa6c86795de6d212b84a498de0aec2935b3d0
Elena Zabolotnaya <elena.zabolotnaya18@gmail.com>
2020-11-25 17:11:10 +0200

    #7 Blocks and templates: added example blocks and passed parameters via layout

---
+++ app/code/Lena/Catalog/Block/Product/View/DemoBlock.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Lena\Catalog\Block;
+use Magento\Tests\NamingConvention\true\string;
+
+/**
+ * @method string getStringVariable()
+ * @method integer getIntVariable()
+ * @method array getArray()
+ */
+class DemoBlock extends \Magento\Framework\View\Element\Template
+{
+
+}
--- dev/tools/grunt/configs/local-themes.js
+++ dev/tools/grunt/configs/local-themes.js
@@ -1,7 +1,18 @@
 /**
  * grunt exec:lena_luma_en_us && grunt less:lena_luma_en_us && grunt watch
+ * grunt exec:lena_luma_uk_ua && grunt less:lena_luma_uk_ua && grunt watch
  */
 module.exports = {
+    lena_luma_uk_ua: {
+        area: 'frontend',
+        name: 'Lena/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     lena_luma_en_us: {
         area: 'frontend',
         name: 'Lena/luma',
