f23a6e35f85ae2ea1506cf6de9987571704596ab
Elena Zabolotnaya <elena.zabolotnaya18@gmail.com>
2021-01-19 12:05:30 +0200

    #12 Model, Resource Model, Collection: updated Request.php

--- app/code/Lena/RegularCustomer/Controller/Index/Request.php
+++ app/code/Lena/RegularCustomer/Controller/Index/Request.php
@@ -18,18 +18,22 @@ class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
      * @var \Magento\Framework\Controller\Result\JsonFactory $jsonResponseFactory
      */
     private $jsonResponseFactory;
+
     /**
      * @var \Lena\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource
      */
     private $discountRequestResource;
+
     /**
      * @var \Lena\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory
      */
     private $discountRequestFactory;
+
     /**
      * @var \Magento\Store\Model\StoreManagerInterface $storeManager
      */
     private $storeManager;
+
     /**
      * @var \Psr\Log\LoggerInterface $logger
      */
@@ -64,8 +68,6 @@ class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
      * https://elena-zabolotnaya-dev.local/lena_regular_customer/index/request
      *
      * @return JsonResponse
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
-     * @throws \Magento\Framework\Exception\AlreadyExistsException
      */
     public function execute(): JsonResponse
     {
@@ -74,8 +76,9 @@ class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
         // @TODO: pass message via notifications, not alert
         // @TODO: add form key validation and hideIt validation
         // @TODO: add Google Recaptcha to the form
-        /** @var DiscountRequest $discountRequest */
+
         try {
+            /** @var DiscountRequest $discountRequest */
             $discountRequest = $this->discountRequestFactory->create();
             $discountRequest->setName($this->request->getParam('name'))
                 ->setEmail($this->request->getParam('email'))
@@ -87,6 +90,7 @@ class Request implements \Magento\Framework\App\Action\HttpPostActionInterface
             $this->logger->error($e->getMessage());
             $message = __('Your request can\'t be sent. Please, contact us if you see this message.');
         }
+
         return $response->setData([
             'message' => $message
         ]);
